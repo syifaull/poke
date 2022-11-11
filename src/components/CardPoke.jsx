2bd33e80e9715c6b1519a075d6afcad48795d883
Syifa Ulyanida <syifaulya0610@gmail.com>
2022-11-11 19:38:24 +0700

    add paginatiion

--- src/components/CardPoke.jsx
+++ src/components/CardPoke.jsx
@@ -1,17 +1,14 @@
-import React from 'react'
-import { Button, Card } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import { Button, Card } from 'react-bootstrap';
 
-const CardPoke = () => {
+const CardPoke = (props) => {
   return (
-    <div>
+    <div className='col'>
       <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Img variant="top" src={props.src} />
         <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
+          <Card.Title>{props.name}</Card.Title>
           <Button variant="primary">Go somewhere</Button>
         </Card.Body>
       </Card>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,61 @@
+import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Button, Container } from 'react-bootstrap';
+import CardPoke from '../components/CardPoke';
 import NavbarComp from '../components/NavbarComp';
 
 const Home = () => {
+  const [pokemon, setPokemon] = useState([]);
+  const [counter, setCounter] = useState(0)
+  const [currentPage, setCurrentPage] = useState([])
 
+  const getPokemon = async (page) => {
+    await axios
+    .get(`https://pokeapi.co/api/v2/pokemon/?offset=${page}&limit=6`)
+    .then((response) => {
+      setPokemon(response.data.results)
+      setCurrentPage(response.data.count)
+
+    })
+    .catch((error) => {
+      alert("error")
+    })
+  }
+
+  useEffect(() => {
+    getPokemon(counter);
+  }, [counter]);
+
+  const previous = () => {
+    setCounter(counter <= 0 ? 0 : counter - 6);
+  }
+
+  const next = () => {
+    // setCounter(counter + 6);
+    setCounter(counter === currentPage ? currentPage : counter + 6)
+  }
 
   return (
     <div>
       <NavbarComp/>
-      // jumbotron
-      <div className=''>
+      {/* jumbotron */}
+      <Container>
+        <div className="row d-flex">
+        {pokemon.map((poke, index) => {
+          return (
+            <CardPoke key={index} src = {poke.url} name = {poke.name}/>
+          )
+        })}
+        </div>
 
-      </div>
+        {/* pagination */}
+        <div>
+          <Button onClick={previous}>Previous</Button>
+          <p>{1 + (counter / 6)}/{Math.ceil(currentPage / 6)}</p>
+          <Button onClick={next}>Next</Button>
+        </div>
+      </Container>
     </div>
   )
 }
